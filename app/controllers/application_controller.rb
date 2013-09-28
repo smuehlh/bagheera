4e690aeae345e58eaa998afdb6e97270b1a9c0d1
stmu <stmu@nmr.mpibpc.mpg.de>
2013-09-28 11:21:03 +0200

    Uncommented error handling in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
 
-	# rescue_from ActionView::Template::Error do |exception|
-	# 	render "error", formats: [:js]
-	# end
+	rescue_from ActionView::Template::Error do |exception|
+		render "error", formats: [:js]
+	end
 end
