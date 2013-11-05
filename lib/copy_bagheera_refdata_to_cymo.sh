161f4b544c45b34f83f0d534bcea439f1782b375
Stefanie Mühlhausen <smuehlh@fab8.(none)>
2013-11-05 10:26:18 +0100

    Added: shell script to copy reference data to cymo

---
+++ lib/copy_bagheera_refdata_to_cymo.sh
@@ -0,0 +1,36 @@
+# !/bin/sh
+
+# copies Bagheera's reference data from fab8 to cymo
+# do not run this script as root (as it will do ssh and scp), but as fab8
+# fab8 has a ssh-key shared with wwwrun@cymo
+
+# initialize variables
+FAB8DATA='/fab8/server/bagheera/db'
+WEBAPPDATA='/usr/local/cymo/bagheera/db'
+DIR='ref_data'
+# reference me like ${DATA}
+
+clean_up () {
+	# housekeeping at program exit
+	ssh wwwrun@cymo "cd ${WEBAPPDATA} && mv ${DIR}_old ${DIR}"
+	exit 	
+}
+
+# save old reference data on cymo
+ssh wwwrun@cymo "cd ${WEBAPPDATA} && mv ${DIR}/ ${DIR}_old"
+
+# scp data to cymo
+scp -rq ${FAB8DATA}/${DIR} wwwrun@cymo:${WEBAPPDATA}
+
+# restore old reference data on cymo in case scp was not successful
+STAT=$?
+if [ $STAT -eq 0 ] ; then
+	# housekeeping on success
+	ssh wwwrun@cymo "cd ${WEBAPPDATA} && rm -rf ${DIR}_old"
+	exit 0
+else
+	# no success 
+	clean_up
+fi
+
+trap clean_up 1 2 3 13 15 
